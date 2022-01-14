9ba2c592399d8a5e39ecb29c26aa29e5d80469e7
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-01-13 23:06:11 -0500

    remove sentence_transformer dependence; calling API

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 
 import json
 
-import numpy as np
-from scipy.spatial.distance import cosine
 import streamlit as st
-from sentence_transformers import SentenceTransformer
+import requests
+
+
+def compute_sentence_similarity(text1, text2):
+    url = "https://y6wrl4j6v7.execute-api.us-east-1.amazonaws.com/default/SentenceSimilarity"
+    payload = json.dumps({'text1': text1, 'text2': text2})
+    headers = {'Content-Type': 'application/json'}
+    response = requests.request('GET', url, headers=headers, data=payload)
+    similarity = response.get('similarity')
+    return similarity
+
 
 demotext1 = 'NIH funds a lot of great science projects.'
 demotext2 = 'A lot of magnificent scientific research projects are funded by NIH.'
-
-smodel = SentenceTransformer('allenai-specter')
 
 st.text('Science Similarity')
 st.write('Disclaimer: This is a demonstration. The models used are of public domain, and are not trained with any private or sensitive data such as PII or PHI.')
@@ -17,8 +23,5 @@ st.write('Disclaimer: This is a demonstration. The models used are of public dom
 text1 = st.text_area('Document 1', demotext1)
 text2 = st.text_area('Document 2', demotext2)
 
-vec1 = smodel.encode(text1)
-vec2 = smodel.encode(text2)
-
-similarity = 1 - cosine(vec1, vec2)
+similarity = compute_sentence_similarity(text1, text2)
 st.write('Similarity: {:.4f}'.format(similarity))
